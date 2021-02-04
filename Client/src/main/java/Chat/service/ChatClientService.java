114e1dab16bc7689ddda5e2e0820f4c5b5584e18
n.simanovschi <nicusimanovschi5>
2021-02-04 16:49:24 +0200

    full refactoring

--- Client/src/main/java/Chat/service/ChatClientService.java
+++ Client/src/main/java/Chat/service/ChatClientService.java
@@ -1,39 +1,8 @@
 package Chat.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface ChatClientService {
+    void sentMessage(String str);
 
-import javax.annotation.PostConstruct;
-
-@Service
-public class ChatClientService {
-
-    @Autowired
-    ChatClient chatClient;
-
-
-    @PostConstruct
-    public void init() {
-        this.connectToServer();
-    }
-
-    public void connectToServer() {
-                this.chatClient.connect();
-    }
-
-    public void sentMessage(String str) {
-        if (verifyState()) {
-            this.chatClient.sent(str);
-        } else {
-            System.out.println("State provide that channel is down");
-        }
-
-    }
-
-
-    public boolean verifyState() {
-        System.out.println("Verify state: " + this.chatClient.state());
-        return this.chatClient.state();
-    }
+    boolean verifyState();
 
 }
